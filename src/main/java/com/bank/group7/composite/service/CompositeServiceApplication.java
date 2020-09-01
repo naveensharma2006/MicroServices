daa0e48b9a6286fe0efa7573519159aff49b5182
naveensharma2006 <57703369+naveensharma2006@users.noreply.github.com>
2020-09-02 02:16:22 +0530

    Changes

--- src/main/java/com/bank/group7/composite/service/CompositeServiceApplication.java
+++ src/main/java/com/bank/group7/composite/service/CompositeServiceApplication.java
@@ -2,6 +2,7 @@ package com.bank.group7.composite.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author naveensharma
