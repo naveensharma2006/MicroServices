395510bdc42901d5bf84df85fab92f8aeec83c5c
naveensharma2006 <57703369+naveensharma2006@users.noreply.github.com>
2020-09-01 22:32:05 +0530

    Comment

--- src/main/java/com/bank/group7/composite/service/CompositeServiceApplication.java
+++ src/main/java/com/bank/group7/composite/service/CompositeServiceApplication.java
@@ -3,6 +3,10 @@ package com.bank.group7.composite.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author naveensharma
+ *
+ */
 @SpringBootApplication
 public class CompositeServiceApplication {
 
